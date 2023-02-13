deac214ac8f8d38333b1a199ad3334b15ee3645d
miradonis <miradonis@web.de>
2023-02-13 11:18:07 +0100

    created Navbar. added Icons from figma

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -2,10 +2,20 @@ import landingImg from '../img/landingImg.png';
 import LoginRegisterImg from '../img/loginRegisterImg.png';
 import BackArrow from '../img/backArrow.png';
 import StartImg from '../img/startImg.png';
+import NavHome from '../img/nav-home.png';
+import NavMeditate from '../img/nav-meditate.png';
+import NavMoon from '../img/nav-moon.png';
+import NavMusic from '../img/nav-music.png';
+import NavProfile from '../img/nav-profile.png';
 
 export {
     landingImg,
     LoginRegisterImg,
     BackArrow,
-    StartImg
+    StartImg,
+    NavHome,
+    NavMeditate,
+    NavMoon,
+    NavMusic,
+    NavProfile
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
 
 const Home = () => {
     return (
         <div>
-
         </div>
     )
 }

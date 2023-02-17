b2c14b0e9a333b5178435bb085ca73331d13cb9b
Marius <Marius.Supercode@gmail.com>
2023-02-17 17:01:23 +0100

    added FavoriteIcon change

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -16,7 +16,7 @@ const Profile = () => {
     const dispatch = useDispatch();
     const userData = useSelector(state => state.user.userData);
     const testUser = {
-        id: "63ebf27631ac83f83b95328f",
+        id: userData._id,
         type: "all"
     };
 
@@ -55,10 +55,10 @@ const Profile = () => {
                 <h4>Favourite Yoga Sessions</h4>
                 <article>
                     {
-                        userFavorites.favorites.filter(element => element.type === 'yoga').map((element, key) => {
+                        userFavorites?.favorites?.filter(element => element.type === 'yoga').map((element, key) => {
                             return (
                                 <SmallCard key={key} image={element.image.url} name={element.name} level={element.level} duration={element.duration} link={`/detail/yoga/${element._id}`} />
-                            )
+                            );
                         })
                     }
                 </article>
@@ -66,15 +66,15 @@ const Profile = () => {
 
             <article className='profileMeditation'>
                 <h4>Favourite Meditations</h4>
-                {/* <div>
+                <div>
                     {
-                        userFavorites.favorites.filter(element => element.type === 'meditation').map((element, key) => {
+                        userFavorites?.favorites?.filter(element => element.type === 'meditation').map((element, key) => {
                             return (
                                 <SmallCard key={key} image={element.image.url} name={element.name} level={element.level} duration={element.duration} />
-                            )
+                            );
                         })
                     }
-                </div> */}
+                </div>
             </article>
             <Navbar page="profile" />
         </section>

11bac325f45335efa82654eb7573ceed6577c443
Marius <Marius.Supercode@gmail.com>
2023-02-18 22:15:02 +0100

    added new AudioPlayer

--- src/components/MusicPopUp/MusicPopUp.jsx
+++ src/components/MusicPopUp/MusicPopUp.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TopNav from '../TopNav/TopNav';
 import './MusicPopUp.scss';
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 import { RHAP_UI } from 'react-h5-audio-player';
+import Player from '../newMusicPlayer/PlayerComp';
 
 
 
@@ -29,10 +30,38 @@ const MusicPopUp = ({ toggleButtonHandler, preview, artist, playlistName, playli
         progressUpdateInterval: 500,
     };
 
+    const [songs, setSongs] = useState(playlist);
+    const [isplaying, setisplaying] = useState(true);
+    const songIndex = playlist.findIndex(x => x.title == preview.title);
+    const [currentSong, setCurrentSong] = useState(playlist[1]);
+
+    const audioElem = useRef();
+
+    useEffect(() => {
+        if (isplaying) {
+            audioElem.current.play();
+        }
+        else {
+            audioElem.current.pause();
+        }
+    }, [isplaying]);
+
+    const onPlaying = () => {
+        const duration = audioElem.current.duration;
+        const ct = audioElem.current.currentTime;
+        setCurrentSong({ ...currentSong, "progress": ct / duration * 100, "length": duration });
+    };
+
     return (
         <section className='musicPopUpSection'>
             <TopNav data="" symbol='arrow' handleClickFunction={toggleButtonHandler} />
-            <AudioPlayer className='audioPlayer' {...playerSettings}></AudioPlayer>
+            {/* <AudioPlayer className='audioPlayer' {...playerSettings}></AudioPlayer> */}
+            <div className="AudioWrapper">
+
+                <audio src={currentSong.preview} ref={audioElem} onTimeUpdate={onPlaying} />
+                <Player songs={songs} setSongs={setSongs} isplaying={isplaying} setisplaying={setisplaying} audioElem={audioElem} currentSong={currentSong} setCurrentSong={setCurrentSong} />
+            </div>
+
         </section>
     );
 };

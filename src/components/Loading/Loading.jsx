1213452f6fa5cdfe75d830364c7bf9b02fe28ca9
Marius-Elting <Marius.Supercode@gmail.com>
2023-05-03 20:09:59 +0200

    fixed userAuth issues, optimized Protected Route

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -2,9 +2,11 @@ import "./Loading.scss";
 
 const Loading = ({ center, customStyle }) => {
 
-    return <div style={customStyle}
-        className={`loading ${center ? "loading-center" : ""}`}>
+    return <div style={customStyle} className={center ? "loading-center" : ""}>
+        <div
+            className={`loading ${center ? "loading-center" : ""}`}>
 
+        </div>
     </div>;
 };
 

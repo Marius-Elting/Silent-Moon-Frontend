b98463b4a23fc9825df2eddf7a4169e157c50dd3
Marius-Elting <Marius.Supercode@gmail.com>
2023-05-03 16:13:24 +0200

    updated images on Landing Page & added Responsive Design for LandingPage

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -10,20 +10,22 @@ const LandingPage = () => {
             <div className='landingPageImg'>
                 <AppHeadline />
             </div>
-            <article className='landingPageArticle'>
-                <h2 className='landingPageHeading'>
-                    We are what we do
-                </h2>
-                <h3 className='landingPageText'>
-                    Thousand of people are using silent moon for meditation and yoga classes.
-                </h3>
-            </article>
-            <SubmitBtn type={"link"} link="/user/register">SIGN UP</SubmitBtn>
-            <article className='landingPageArticle'>
-                <h3>
-                    ALREADY HAVE AN ACCOUNT? <Link to="/user/login" className='landingPageLink'>LOG IN</Link>
-                </h3>
-            </article>
+            <section className='landingBottomWrapper'>
+                <article className='landingPageArticle'>
+                    <h2 className='landingPageHeading'>
+                        We are what we do
+                    </h2>
+                    <h3 className='landingPageText'>
+                        Thousand of people are using silent moon for meditation and yoga classes.
+                    </h3>
+                </article>
+                <SubmitBtn type={"link"} link="/user/register">SIGN UP</SubmitBtn>
+                <article className='landingPageArticle'>
+                    <h3>
+                        ALREADY HAVE AN ACCOUNT? <Link to="/user/login" className='landingPageLink'>LOG IN</Link>
+                    </h3>
+                </article>
+            </section>
 
         </section>
     )
